727436ad099452cd8d63487a6e697c25c1ffa5a4
visanz2 <visanz2@gmail.com>
2025-01-30 15:37:26 +0100

    project draft

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import CardProcessor from './components/CardProcessor'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <CardProcessor />
+    </div>
+  )
+}
+
+export default App
